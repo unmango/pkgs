aaf8a133f425e64c64292f022a2a1c44c3e420d6
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-15 19:25:54 -0500

    Flake init

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Mini-nixpkgs of dubious quality";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              gnumake
+              nixfmt
+            ];
+          };
+
+          treefmt.programs = {
+            nixfmt.enable = true;
+          };
+        };
+    };
+}
