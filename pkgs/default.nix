4a2a197fbfdbcff9600b5b55e4cb1affdbd059a8
UnstoppableMango <erik.rasmussen@unmango.dev>
2026-06-30 11:04:14 -0500

    Package oc-mirror

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -26,6 +26,7 @@
         kubectl-get-resources = callPackage ./kubectl-get-resources { };
         kubectl-slice = callPackage ./kubectl-slice { };
         mmake = callPackage ./mmake { };
+        oc-mirror = callPackage ./oc-mirror { };
         pbrt = callPackage ./pbrt { };
         ocaml-protoc = callPackage ./ocaml-protoc { inherit (config.packages) pbrt; };
         ocaml-protoc-plugin = callPackage ./ocaml-protoc-plugin { };
@@ -61,6 +62,7 @@
           kubectl-get-resources
           kubectl-slice
           mmake
+          oc-mirror
           ocaml-protoc
           ocaml-protoc-plugin
           openshift-installer
